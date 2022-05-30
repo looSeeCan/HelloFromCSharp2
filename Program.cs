ca2eef22392e7bb61eae28516443ae431ee1a151
Lucycan LY <95648904+looSeeCan@users.noreply.github.com>
2022-05-30 10:56:12 -0500

    console.writeline is cool. i wonder if js has this method

--- Program.cs
+++ Program.cs
@@ -1,3 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Everyone!");
 
+Console.WriteLine("Please enter your first name: ");
+string firstName = Console.ReadLine();//cool. the console reads the line.
+
+Console.WriteLine("Please enter your last name");
+string lastName = Console.ReadLine();
+
+Console.WriteLine($"Hello {firstName} {lastName}");
